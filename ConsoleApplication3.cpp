cdf976de81291083304bec3c5257b869e4a240e9
Stephan Michailenko <94607203+stephan-art@users.noreply.github.com>
2022-10-16 17:53:04 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int W, H, R, D;
+	cin >> W >> H >> R;
+	D = 2 * R;
+	if (D <= W && D <= H) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+
+}
